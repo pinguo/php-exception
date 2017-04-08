d8d429355a532d5fd571cfba3e8a8b66a7aafc4a
pinguo-xiaoshiyong <xiaoshiyong@camera360.com>
2017-04-08 15:07:10 +0800

    add privilege exception

---
+++ src/PrivilegeException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * 无权限操作
+ *
+ * @author camera360_server@camera360.com
+ * @copyright Chengdu pinguo Technology Co.,Ltd.
+ */
+namespace PG\Exception;
+
+class PrivilegeException extends \ErrorException
+{
+    public function __construct($message, $code = Errno::PRIVILEGE_NOT_PASS)
+    {
+        parent::__construct($message, $code);
+    }
+}
